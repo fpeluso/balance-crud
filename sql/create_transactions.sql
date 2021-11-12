7adaeb3f712180d481c216b3eab84c8ccfd7448d
Fabio Peluso <fabio.peluso@smc.it>
2021-11-12 17:30:27 +0100

    aggiunto account (origine/destinazione) della spesa/incasso

--- sql/create_transactions.sql
+++ sql/create_transactions.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS public.transactions
     id SERIAL PRIMARY KEY,
     transactionType transaction_type NOT NULL,
     amount integer NOT NULL,
+    account character(255)[] COLLATE pg_catalog."default" NOT NULL,
     category character(255)[] COLLATE pg_catalog."default" NOT NULL,
     "transactionDate" date NOT NULL,
     description character(255)[] COLLATE pg_catalog."default"
